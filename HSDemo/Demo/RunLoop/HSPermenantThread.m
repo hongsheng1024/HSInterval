9416776f3ffc054012f9a8684f7921ad66d80988
whs <wwanghongsheng@cebvendor.com>
2020-09-08 13:25:00 +0800

    线程保活

---
+++ HSDemo/Demo/RunLoop/HSPermenantThread.m
@@ -0,0 +1,13 @@
+//
+//  HSPermenantThread.m
+//  Demo
+//
+//  Created by FaceBook on 2020/9/8.
+//  Copyright © 2020 whs. All rights reserved.
+//
+
+#import "HSPermenantThread.h"
+
+@implementation HSPermenantThread
+
+@end
---
+++ HSDemo/Demo/RunLoop/HSThread.m
@@ -0,0 +1,13 @@
+//
+//  HSThread.m
+//  Demo
+//
+//  Created by FaceBook on 2020/9/8.
+//  Copyright © 2020 whs. All rights reserved.
+//
+
+#import "HSThread.h"
+
+@implementation HSThread
+
+@end
